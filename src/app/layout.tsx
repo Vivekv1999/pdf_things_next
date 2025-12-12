62a45d5263689becac86b0ae36475a4e1ed4ff08
Vivel PC <vivekvekariya1999@gmail.com>
2025-12-13 04:09:59 +0530

    enter correct key for GA_MEASUREMENT_ID

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -64,13 +64,13 @@ export default function RootLayout({
       >
 
         {/* Google Analytics */}
-        <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}></script>
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_MEASUREMENT_ID}`}></script>
         <script dangerouslySetInnerHTML={{
           __html: `
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
-                gtag('config', ${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID});
+                gtag('config', ${process.env.GA_MEASUREMENT_ID});
               `
         }} />
 
