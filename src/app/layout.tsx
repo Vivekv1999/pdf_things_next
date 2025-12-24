f642dcbcfa13c9bceaebe0ae15b54713d43e08d9
Vivel PC <vivekvekariya1999@gmail.com>
2025-12-25 00:18:17 +0530

    Remove google anatic form script and add google tag manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -67,27 +67,37 @@ export default function RootLayout({
         {/* GA4 Config */}
         {process.env.NEXT_PUBLIC_ENVIRONMENT === "production" && (
           <Script
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_MEASUREMENT_ID}`}
+            id="gtm-script"
             strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${process.env.GT_MANAGER_ID}');
+            `,
+            }}
           />
         )}
 
-        <Script id="ga-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${process.env.GA_MEASUREMENT_ID}', {
-              page_path: window.location.pathname,
-            });
-          `}
-        </Script>
       </head>
 
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-
+        {/* <!-- Google Tag Manager (noscript) --> */}
+        {process.env.NEXT_PUBLIC_ENVIRONMENT === "production" && (
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=${process.env.GT_MANAGER_ID}`}
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            />
+          </noscript>
+        )}
+        {/* <!-- End Google Tag Manager (noscript) --> */}
 
         {/* JSON-LD Structured Data for SEO */}
         <Script
